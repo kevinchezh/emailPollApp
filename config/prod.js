7382c439183b82185a18b13d8bd445cdb98b973f
Zhenhao Che <chezh@seas.upenn.edu>
2018-10-27 21:24:39 -0400

    add a comment

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 module.exports={
+    //heroku env variable
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
     mongoURI : process.env.MONGO_URI,
