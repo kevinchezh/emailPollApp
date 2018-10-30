78a0f48535567d56a843ba822e0111407bf5cd8e
Zhenhao Che <chezh@seas.upenn.edu>
2018-10-29 22:00:56 -0400

    small typo

--- index.js
+++ index.js
@@ -40,9 +40,9 @@ if(process.env.NODE_ENV==='production'){
     //express will serve up the index.html file if is does not
     //recognize route
     const path = require('path');
-    app.get('/*', (req,res) => {
+    app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    })
+    });
 }
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
